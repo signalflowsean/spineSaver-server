2f8a4a92b34b509fb852612bb5db1e28a0095f1a
Sean Nealon <signalflowsean@gmail.com>
2018-11-20 16:27:58 -0500

    starting to add db

--- config.js
+++ config.js
@@ -3,9 +3,11 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
-  DATABASE_URL:
-        process.env.DATABASE_URL || 'mongodb://localhost/thinkful-backend',
+  
+  DATABASE_URL: 
+      process.env.DATABASE_URL || 'mongodb://localhost/spine-saver-backend',
+  
   TEST_DATABASE_URL:
-        process.env.TEST_DATABASE_URL ||
-        'mongodb://localhost/thinkful-backend-test'
+      process.env.TEST_DATABASE_URL ||
+      'mongodb://localhost/spine-saver-backend-test'
 }; 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser'); 
 const cors = require('cors'); 
 const morgan = require('morgan'); 
+const mongoose = require('mongoose'); 
 
 const { PORT, CLIENT_ORIGIN } = require('./config'); 
 
@@ -30,7 +31,7 @@ app.get('/api/hi', (req, res) => {
 app.post('/api/slouchData', (req, res) => { 
   const {slouchData} = req.body; 
 
-  res.json({slouchData}); 
+  //res.json({slouchData}); 
 }); 
 
 function runServer(port = PORT) { 
@@ -41,14 +42,10 @@ function runServer(port = PORT) {
     .on('error', err => { 
       console.err('Express failed:', err); 
     }); 
-
 }
 
 if (require.main === module) { 
-  //dbConnect();
   runServer(); 
 }
 
-
-//app.listen(8080); 
 
