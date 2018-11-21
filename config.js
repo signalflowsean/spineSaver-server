d5aaf3e6997bd9221c04e35f5868e720bb73175e
Sean Nealon <signalflowsean@gmail.com>
2018-11-21 13:56:47 -0500

    updated mongoose env url

--- config.js
+++ config.js
@@ -5,9 +5,9 @@ module.exports = {
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
   
   DATABASE_URL: 
-      process.env.DATABASE_URL || 'mongodb://localhost/spine-saver-backend',
+      process.env.DATABASE_URL || 'mongodb://localhost/spine-saver-db',
   
   TEST_DATABASE_URL:
       process.env.TEST_DATABASE_URL ||
-      'mongodb://localhost/spine-saver-backend-test'
+      'mongodb://localhost/spine-saver-db-test'
 }; 
